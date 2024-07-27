f260f335496e952d32697cfa84b65c5c1c3d607b
VineethChivukula <vineethonsocialmedia@gmail.com>
2024-07-27 14:54:05 +0530

    commit

--- array/GasStation.java
+++ array/GasStation.java
@@ -29,7 +29,74 @@
  * 
  * Space Complexity Analysis:
  * The Space Complexity of this approach is O(1) since we are not using any
- * extra space. 
+ * extra space.
  * 
+ * Efficient Approach:
+ * The concept here is very interesting to observe. What we will do is that we
+ * calculate the total gas balance right from the starting index. We keep track
+ * of this balance in order to check if we can complete the circuit. We are
+ * doing this in order to avoid the extra loop that we were running in the above
+ * approach. We will run a loop from 0 to n. Inside the loop, we will calculate
+ * the gas[i] - cost[i] and store it in the totalGasBalance. We will also
+ * calculate the currentGasBalance as currentGasBalance + gas[i] - cost[i].
+ * currentGasBalance will keep track of the gas balance from the ith station to
+ * the future stations. If currentGasBalance is less than 0, we will set
+ * currentGasBalance to 0 and set startIndex to i+1. We will return startIndex
+ * if totalGasBalance is greater than or equal to 0. We will return -1 if we
+ * come out of the loop.
  * 
+ * Time Complexity Analysis:
+ * The Time Complexity of this approach is O(n) since we are running a single
+ * loop.
+ * 
+ * Space Complexity Analysis:
+ * The Space Complexity of this approach is O(1) since we are not using any
+ * extra space.
  */
+
+public class GasStation {
+	public int approach1(int[] gas, int[] cost) {
+		int n = gas.length;
+		for (int startIndex = 0; startIndex < n; startIndex++) {
+			int travelCost = 0;
+			int travelIndex = startIndex;
+			boolean canComplete = true;
+			for (int i = 0; i < n; i++) {
+				travelCost = travelCost + gas[travelIndex] - cost[travelIndex];
+				if (travelCost < 0) {
+					canComplete = false;
+					break;
+				}
+				travelIndex = (travelIndex + 1) % n;
+			}
+			if (canComplete) {
+				return startIndex;
+			}
+		}
+		return -1;
+	}
+
+	public int efficientApproach(int[] gas, int[] cost) {
+		int n = gas.length;
+		int totalGasBalance = 0;
+		int currentGasBalance = 0;
+		int startIndex = 0;
+		for (int i = 0; i < n; i++) {
+			totalGasBalance += gas[i] - cost[i];
+			currentGasBalance += gas[i] - cost[i];
+			if (currentGasBalance < 0) {
+				currentGasBalance = 0;
+				startIndex = i + 1;
+			}
+		}
+		return totalGasBalance >= 0 ? startIndex : -1;
+	}
+
+	public static void main(String[] args) {
+		GasStation gs = new GasStation();
+		int[] gas = { 1, 2, 3, 4, 5 };
+		int[] cost = { 3, 4, 5, 1, 2 };
+		// System.out.println(gs.approach1(gas, cost));
+		System.out.println(gs.efficientApproach(gas, cost));
+	}
+}
